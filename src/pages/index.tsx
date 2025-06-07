37af3219b2ea020acfaa1714a2de217ae64fbe77
LaggySon <43mikester@gmail.com>
2025-06-07 18:17:47 -0400

    start adding images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,23 +17,48 @@ export const getStaticProps = (async (context) => {
   const options: Option[] = [
     {
       name: "Merab Dvalishvili",
-      image: "https://placehold.co/400x400?text=1",
+      image:
+        "https://a.espncdn.com/combiner/i?img=/i/headshots/mma/players/full/3948572.png&w=400&h=400",
     },
     {
       name: "Sean O'Malley",
-      image: "https://placehold.co/400x400?text=2",
+      image:
+        "https://a.espncdn.com/combiner/i?img=/i/headshots/mma/players/full/4205093.png&w=400&h=400",
     },
     {
       name: "Aljamain Sterling",
-      image: "https://placehold.co/400x400?text=3",
+      image:
+        "https://a.espncdn.com/combiner/i?img=/i/headshots/mma/players/full/3031559.png&w=400&h=400",
     },
     {
       name: "Cory Sandhagen",
-      image: "https://placehold.co/400x400?text=4",
+      image:
+        "https://a.espncdn.com/combiner/i?img=/i/headshots/mma/players/full/4294504.png&w=400&h=400",
     },
     {
       name: "Petr Yan",
-      image: "https://placehold.co/400x400?text=5",
+      image:
+        "https://a.espncdn.com/combiner/i?img=/i/headshots/mma/players/full/4293517.png&w=400&h=400",
+    },
+    {
+      name: "Marlon Vera",
+      image: "https://static.ufcstats.com/images/marlon-vera.png",
+    },
+    {
+      name: "Dominick Cruz",
+      image: "https://static.ufcstats.com/images/dominick-cruz.png",
+    },
+    {
+      name: "Rob Font",
+      image: "https://static.ufcstats.com/images/rob-font.png",
+    },
+    {
+      name: "Ricky Simon",
+      image: "https://static.ufcstats.com/images/ricky-simon.png",
+    },
+    {
+      name: "Song Yadong",
+      image: "https://static.ufcstats.com/images/song-yadong.png",
     },
   ];
   return {
@@ -47,8 +72,8 @@ export default function Home({
   options,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const [ranks, setRanks] = useState(
-    options.map((option, index) => ({
-      title: index,
+    Array.from({ length: 3 }, (_, index) => ({
+      title: index + 1,
       name: "",
       image: "",
     }))
@@ -59,7 +84,7 @@ export default function Home({
     setNextRank(options[Math.floor(Math.random() * options.length)]);
   });
 
-  function rankOption(index: number, nextRank: Option): void {
+  function rankOption(index: number, nextRank: Option | undefined): void {
     if (!nextRank) return;
     const nextRankIndex = options.findIndex((option) => option == nextRank);
     console.log(nextRankIndex);
@@ -73,20 +98,34 @@ export default function Home({
   return (
     <main className={styles.main}>
       <div className={styles.rankBox}>
-        {ranks.map((rank, index) => (
-          <div
-            className={styles.rank}
-            onClick={() => rankOption(index, nextRank)}
-          >
-            <div className={styles.title}>{rank.title}</div>
-            <div className={styles.name}>{rank.name}</div>
-            <div className={styles.image}>{rank.image}</div>
-          </div>
-        ))}
+        {ranks.map((rank, index) => {
+          // Generate a color in HSL space for a smooth gradient
+          const hue = Math.round((index / (ranks.length * 2)) * 360);
+          const backgroundColor = `hsl(${hue}, 70%, 60%)`;
+          return (
+            <div key={index} className={styles.rank}>
+              <div
+                onClick={() => rankOption(index, nextRank)}
+                className={styles.title}
+                style={{ backgroundColor }}
+              >
+                {rank.title}
+              </div>
+              {rank.image && (
+                <img className={styles.image} src={rank.image} alt="" />
+              )}
+
+              <div className={styles.name}>{rank.name}</div>
+            </div>
+          );
+        })}
       </div>
       <div className={styles.nextToRank}>
         <div className={styles.rank} id={styles.next}>
-          <div className={styles.content}>{nextRank?.name || "DONE!!"}</div>
+          {nextRank?.image && (
+            <img className={styles.image} src={nextRank.image} alt="" />
+          )}
+          <div className={styles.name}>{nextRank?.name || "DONE!!"}</div>
         </div>
       </div>
     </main>
